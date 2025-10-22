3a9fd1c9933297f5cb5e6d30ac5f5aed81964406
Bastien <bastienryser20004@gmail.com>
2025-10-22 21:12:34 +0200

    go

--- src/i18n.js
+++ src/i18n.js
@@ -1,9 +1,9 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-import en from './locales/en.json';
-import fr from './locales/fr.json';
+import en from "./locales/en.json";
+import fr from "./locales/fr.json";
 
 const resources = {
   en: { translation: en },
@@ -15,13 +15,13 @@ i18n
   .use(initReactI18next)
   .init({
     resources,
-    fallbackLng: 'fr',
+    fallbackLng: "en",
     interpolation: {
       escapeValue: false,
     },
     detection: {
-      order: ['localStorage', 'navigator'],
-      caches: ['localStorage'],
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
     },
   });
 
